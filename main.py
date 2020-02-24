01c1acfb3a9f5cb68dfbc63f59cccc816c4b0955
Amanda Zheng <azheng00@stuy.edu>
2020-02-24 00:12:53 -0500

    fixing picture

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("\nimg.png\n\n")
 s = new_screen()
 c = [ 0, 255, 0 ]
 print("matrix m1=:\n")
-matrix = new_matrix()
+matrix = new_matrix(4,0)
 print_matrix(matrix)
 #p1 45,390
 #p2 452,390
@@ -51,26 +51,26 @@ ident(identity)
 print_matrix(identity)
 
 print("\ntesting add edge. Adding: (50 450 0)(100 450 0) to m1:\n")
-add_edge(matrix, 50, 450, 0, 100, 450, 0);
+add_edge(matrix, 50, 450, 0, 100, 450, 0)
 print_matrix(matrix)
 print("\nadded more edges\n")
-add_edge(matrix, 50, 450, 0, 50, 400, 0);
-add_edge(matrix, 100, 450, 0, 100, 400, 0);
-add_edge(matrix, 100, 400, 0, 50, 400, 0);
+add_edge(matrix, 50, 450, 0, 50, 400, 0)
+add_edge(matrix, 100, 450, 0, 100, 400, 0)
+add_edge(matrix, 100, 400, 0, 50, 400, 0)
 
-add_edge(matrix, 200, 450, 0, 250, 450, 0);
-add_edge(matrix, 200, 450, 0, 200, 400, 0);
-add_edge(matrix, 250, 450, 0, 250, 400, 0);
-add_edge(matrix, 250, 400, 0, 200, 400, 0);
+add_edge(matrix, 200, 450, 0, 250, 450, 0)
+add_edge(matrix, 200, 450, 0, 200, 400, 0)
+add_edge(matrix, 250, 450, 0, 250, 400, 0)
+add_edge(matrix, 250, 400, 0, 200, 400, 0)
 
-add_edge(matrix, 150, 400, 0, 130, 360, 0);
-add_edge(matrix, 150, 400, 0, 170, 360, 0);
-add_edge(matrix, 130, 360, 0, 170, 360, 0);
+add_edge(matrix, 150, 400, 0, 130, 360, 0)
+add_edge(matrix, 150, 400, 0, 170, 360, 0)
+add_edge(matrix, 130, 360, 0, 170, 360, 0)
 
-add_edge(matrix, 100, 340, 0, 200, 340, 0);
-add_edge(matrix, 100, 320, 0, 200, 320, 0);
-add_edge(matrix, 100, 340, 0, 100, 320, 0);
-add_edge(matrix, 200, 340, 0, 200, 320, 0);
+add_edge(matrix, 100, 340, 0, 200, 340, 0)
+add_edge(matrix, 100, 320, 0, 200, 320, 0)
+add_edge(matrix, 100, 340, 0, 100, 320, 0)
+add_edge(matrix, 200, 340, 0, 200, 320, 0)
 print("\n after adding a lot of edges, m1:")
 print_matrix(matrix)
 
